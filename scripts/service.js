e2506e8ac7f289d06c28a454ac613c7d97475204
mehmetranas <mhmtranas@gamil.com>
2017-11-11 23:11:40 +0300

    Added full link and not-found div

--- scripts/service.js
+++ scripts/service.js
@@ -1,7 +1,7 @@
 var wikiService = function (searchTerm, callback) {
     $(".pending").show();
   var url =
-      "https://" + lang + ".wikipedia.org" + "/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&piprop=original&indexpageids=1&generator=search&exsentences=3&exintro=1&explaintext=1&gsrlimit=10&gsrsearch=" + searchTerm;
+      "https://" + lang + ".wikipedia.org" + "/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages%7Cinfo&piprop=original&inprop=url&indexpageids=1&generator=search&exsentences=3&exintro=1&explaintext=1&gsrlimit=10&gsrsearch=" + searchTerm;
 
     $.ajax({
        method:"POST",
